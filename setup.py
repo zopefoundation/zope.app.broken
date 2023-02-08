628b6dac6d2f8d2d8fcaa74a664e80ce09933d79
Michael Howitz <mh@gocept.com>
2023-02-08 16:05:22 +0100

    Preparing release 5.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def read(*rnames):
 
 
 setup(name='zope.app.broken',
-      version='5.0.dev0',
+      version='5.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope Broken (ZODB) Object Support',
